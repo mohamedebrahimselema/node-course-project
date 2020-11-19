96fa9b6cdcc1ebf02c88245f1e97e8140371f990
mohamedibrahemselema <mohamed.ibrahiem5905@ci.menofia.edu.eg>
2020-11-19 19:44:15 +0200

    express with routes

--- server_express.js
+++ server_express.js
@@ -6,9 +6,26 @@ dotenv.config({ path: './config/config.env'});
 const app = express();
 
 
+app.get('/api/v1/bootcamp/', (req, res) => {
+    res.status(200).json({sucess: true , message : "show all bootcamps"});
+});
 
 
+app.post('/api/v1/bootcamp/', (req, res) => {
+    res.status(200).json({sucess: true , message : "create new bootcamps"});
+});
+app.get('/api/v1/bootcamp/:id', (req, res) => {
+    res.status(200).json({sucess: true , message : `display bootcamp get  ${req.params.id}`});
+});
 
+app.put('/api/v1/bootcamp/:id', (req, res) => {
+    res.status(200).json({sucess: true , message : `update bootcamp put  ${req.params.id}`});
+});
+
+
+app.delete('/api/v1/bootcamp/:id', (req, res) => {
+    res.status(200).json({sucess: true , message : `delete  bootcamps  ${req.params.id}`});
+});
 
 const PORT = process.env.PORT || 5000 ;
 app.listen(PORT, () => {
